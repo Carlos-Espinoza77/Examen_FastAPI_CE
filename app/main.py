0588a042004d99d288172470be06d9228707766a
Carlos-Espinoza77 <ecarlosi1977@gmail.com>
2026-07-31 16:10:53 -0500

    Feat: datos iniciales de main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from examen-fasapi-ce!")
+#Importa la clase principal que permite la aplicación web de FastAPI
+from fastapi import FastAPI
 
+#Se crea la instancia principal de la aplicacion FASTAPI
+app = FastAPI(
+    title="Examen FastAPI",
+    description="API REST de usuarios, publicaciones y comentarios",
+    version="1.0.0", #versión inicial del proyecto a implementar
+)
 
-if __name__ == "__main__":
-    main()
+#Corresponde a la ruta principal de la aplicación
+@app.get("/")
+def inicio() -> dict[str, str]: #indica que la función devuelve un diccionario con claves y valores de texto
+    return {
+        "message": "La aplicación FastAPI funciona correctamente" #FASTAPI convierte automáticamente este diccionario en JSON
+    }
