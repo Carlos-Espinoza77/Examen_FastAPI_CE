6b185e862b6c60e3b6bbb225951cf16c3617c1e4
Carlos-Espinoza77 <ecarlosi1977@gmail.com>
2026-07-31 18:33:54 -0500

    Feat: adicionando en models.py

--- app/database/models.py
+++ app/database/models.py
@@ -10,3 +10,25 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100))
     email: Mapped[str] = mapped_column(String(150), unique=True)
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(200))
+    content: Mapped[str] = mapped_column(String)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="posts")
+    comments = relationship("Comment", back_populates="post")
+
+class Comment(Base):
+    __tablename__ = "comments"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    content: Mapped[str] = mapped_column(String)
+
+    post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"))
+
+    post = relationship("Post", back_populates="comments")
+
