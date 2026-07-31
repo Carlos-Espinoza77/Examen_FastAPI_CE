9b5df77a8811addfd9665d85ac28051a8a912912
Carlos-Espinoza77 <ecarlosi1977@gmail.com>
2026-07-31 18:28:15 -0500

    Feat: modificando models

--- app/database/models.py
+++ app/database/models.py
@@ -2,3 +2,11 @@ from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(150), unique=True)
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI
 
 from app.config import settings
+from app.database import models
 from app.database.database import Base, engine
 
 #Se crea la instancia principal de la aplicacion FASTAPI
