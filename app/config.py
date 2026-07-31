435284379c7cac0b1bda195039c55da6e9e65049
Carlos-Espinoza77 <ecarlosi1977@gmail.com>
2026-07-31 17:16:24 -0500

    Feat: Agregando en config.py lectura archivo

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -11,3 +11,10 @@ class Settings(BaseSettings):
     secret_key: str
 
     database_url: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+settings = Settings()
