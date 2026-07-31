415ee86741e5f8d27a0be9ba63f94b1934f45084
Carlos-Espinoza77 <ecarlosi1977@gmail.com>
2026-07-31 18:36:49 -0500

    Feat: Schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr
+
+class UserBase(BaseModel):
+    name: str
+    email: EmailStr
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    id: int
+
+    model_config = {
+        "from_attributes": True
+    }
