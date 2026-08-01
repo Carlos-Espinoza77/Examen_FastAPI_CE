98fa0dd4ca28ec1fe5bb23e1a433359912918dd4
Carlos-Espinoza77 <ecarlosi1977@gmail.com>
2026-07-31 19:10:13 -0500

    Feat: Crud de usuario

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from app.config import settings
 from app.database import models
 from app.database.database import Base, engine
+from app.routes.users import router as user_router
+
 
 #Se crea la instancia principal de la aplicacion FASTAPI
 app = FastAPI(
@@ -13,6 +15,7 @@ app = FastAPI(
 )
 
 Base.metadata.create_all(bind=engine)
+app.include_router(user_router)
 
 #Corresponde a la ruta principal de la aplicación
 @app.get("/")
---
+++ app/routes/users.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.database.database import get_db
+from app.schemas.user import UserCreate, UserResponse
+from app.services.user_service import UserService
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@router.get("/", response_model=list[UserResponse])
+def get_users(db: Session = Depends(get_db)):
+    return UserService.get_all(db)
+
+
+@router.post("/", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    return UserService.create(db, user)
+
